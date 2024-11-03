8e734c7c21abea26314682de195a209b4ab9bcd0
webxrahat <islammdrahat308@gmail.com>
2024-11-03 14:55:01 +0600

    rr

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Basic Template @yield('title')</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Basic Template @yield('title', 'web site')</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
    
 </head>
 <body>
@@ -43,4 +43,4 @@
     </footer>
   
 </body>
-</html>
+</html> 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,5 +5,6 @@
 @endsection
 
 @section('content')
- 
+  <h1>Post Page Title</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, distinctio?</p>
 @endsection
